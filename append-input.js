ccd4fd53cdd66a84c0d6393d571e1150dae34e06
glitchgirl <londonspleen@gmail.com>
2019-10-07 15:35:21 -0400

    examples for tiny servers

--- append-input.js
+++ append-input.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const readline = require('readline');
+var events = require('events');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let lyrics = rl.question("add text?", (answer)=>{
+rl.question("add text?", (answer)=>{
 	// write to a new file named text.txt
 fs.writeFile('test.txt', answer, (err) => {
   // throws an error, you could also catch it here
